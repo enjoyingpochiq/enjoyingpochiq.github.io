470dd92e8919f45c6bf5bc440f11b5039ffe3c03
ahervas <pepelombarde@gmail.com>
2026-03-31 13:05:47 +0200

    🚀 Configurar despliegue automático con GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import remarkBreaks from 'remark-breaks';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://enjoyingpochiq.github.io', 
   vite: {
     plugins: [tailwindcss()]
+  },
+  markdown: {
+    remarkPlugins: [remarkBreaks],
   }
 });
